64c6777ef92f9e2d7fed3c9fe503f6a95f58fd6c
Ross Perkel <rossperkel@gmail.com>
2019-09-27 17:45:42 -0400

    sorta thru auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+
+    def encode_token(payload)
+        JWT.encode payload, 'iLoveButtsAndTrivia', 'HS256'
+    end
+
+    def user_payload(user)
+        {user_id: user.id}
+    end
+    
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :game_boards
   resources :questions
   resources :users
+
+  post '/login', to: "auth#login"
+  post '/signup', to: "users#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
